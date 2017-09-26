8ab3461ed5821e0f0c219012a8cbc3795af1e343
folkvir <dev.arnaudgrall@gmail.com>
2017-09-26 17:05:18 +0200

    sondage updated to 3.0

--- example/sondage/main.js
+++ example/sondage/main.js
@@ -1,12 +1,31 @@
-var Foglet = require("foglet").Foglet;
+localStorage.debug = 'foglet-core:*';
 
-var foglet,spray,sondage,votesData;
+const Foglet = require('foglet').Foglet;
+
+var foglet,spray,register,votesData, svg;
 let f;
 $(document).ready(function() {
 	$('#survey').hide();
 	$('#connection').show();
 });
 
+/**
+* Create a register named sondage
+* @param {[type]} "sondage" [description]
+*/
+register = {
+  sondage: [0,0]
+}
+
+let intervalFirstValue;
+
+/*
+* Votes data structure for d3
+*/
+votesData=[
+  {label:"Oui", color:"#2AABD2"},
+  {label:"Non", color:"#265A88"}
+];
 
 /**
 Http request in order to get list of stun servers
@@ -32,74 +51,98 @@ $.ajax({
      if(response.d.iceServers){
        iceServers = response.d.iceServers;
      }
-    var foglet = new Foglet({
-    	room: 'survey',
-			protocol: 'survey',
-      signalingAdress : "https://signaling.herokuapp.com/",
-			wrtc:{
-				trickle: true,
-				iceServers: iceServers
-			}
+
+
+    f = new Foglet({
+      verbose: true, // want some logs ? switch to false otherwise
+      rps: {
+        type: 'spray-wrtc',
+        options: {
+          protocol: 'survey', // foglet running on the protocol foglet-example, defined for spray-wrtc
+          webrtc:	{ // add WebRTC options
+            trickle: true, // enable trickle (divide offers in multiple small offers sent by pieces)
+            iceServers : iceServers // define iceServers in non local instance
+          },
+          timeout: 2 * 60 * 1000, // spray-wrtc timeout before definitively close a WebRTC connection.
+          delta: 60 * 1000, // spray-wrtc shuffle interval
+          signaling: {
+            address: 'https://signaling.herokuapp.com/',
+            // signalingAdress: 'https://signaling.herokuapp.com/', // address of the signaling server
+            room: 'survey' // room to join
+          }
+        }
+      }
     });
 
-    f = foglet;
 
-    foglet.connection().then( () => {
+
+    // engage the share signaling process
+    f.share();
+
+    // connection to the network throught the signaling server
+    f.connection().then( () => {
 			console.log('Your are now connected.');
-			/**
-	     * Create a register named sondage
-	     * @param {[type]} "sondage" [description]
-	     */
-	    foglet.addRegister("sondage");
 
-			/**
-	     * Get the register
-	     * @param  {[type]} "sondage" [description]
-	     * @return {[type]}        [description]
-	     */
-	    sondage = foglet.getRegister("sondage");
+      // once connected engage the process to retrieve old data
+      intervalFirstValue = setInterval(() => {
+        // send a request to only one peer
+        let randomPeer = f.getRandomNeighbourId();
+        console.log(randomPeer);
+        if(randomPeer) f.sendUnicast(randomPeer, formatMessage('request-votes', {}));
+      }, 2000);
 
-	    /**
-	     * Listening on the signal sondage-receive where every data are sent when the register is updated.
-	     * @param  {[type]} "sondage-receive" [description]
-	     * @param  {[type]} function(data  [description]
-	     * @return {[type]}                [description]
-	     */
-	    sondage.on("sondage-receive",function(data){
-	      changeData(sondage.getValue());
-	    });
-	    /**
-	     * Set its value, and send by broadcast
-	     * @param {[type]}
-	     */
-	    sondage.setValue([0,0]);
-	    /*
-	    * Votes data structure for d3
-	    */
-	    votesData=[
-	    	{label:"Oui", color:"#2AABD2"},
-	    	{label:"Non", color:"#265A88"}
-	    ];
-	    /*
-	    * Create the canvas in chart id html tag
-	    * and draw the pieGraph
-	    */
-	    var svg = d3.select("#chart").append("svg").attr("width",300).attr("height",300);
-	    svg.append("g").attr("id","quotesDonut");
-	    Donut3D.draw("quotesDonut", updateData(sondage.getValue()), 150, 150, 130, 100, 30, 0);
-
-			$('#survey').show();
-			$('#connection').hide();
 		});
+
+    f.onUnicast((id, msg) => {
+      console.log('Receive unicast message: ', id, msg);
+      if(msg.type && msg.type === 'request-votes'){
+        // respond with the register
+        console.log('Respond with our register value. ', id, msg);
+        f.sendUnicast(id, formatMessage('answer-votes', register));
+
+      } else if (msg.type && msg.type === 'answer-votes') {
+        if(intervalFirstValue) clearInterval(intervalFirstValue);
+        // update the local register
+        console.log(msg.value.sondage);
+        setVotes(msg.value.sondage);
+
+        showInterface();
+      }
+    });
+
+    f.onBroadcast((id, msg) => {
+      if (msg.type && msg.type === 'update'){
+        console.log('Receive an updated value: ', msg.value.sondage);
+        setVotes(msg.value.sondage);
+      }
+    });
   }
 });
+
+function showInterface() {
+
+  /*
+  * Create the canvas in chart id html tag
+  * and draw the pieGraph
+  */
+  svg = d3.select("#chart").append("svg").attr("width",300).attr("height",300);
+  svg.append("g").attr("id","quotesDonut");
+  Donut3D.draw("quotesDonut", updateData(register.sondage), 150, 150, 130, 100, 30, 0);
+
+  $('#survey').show();
+  $('#connection').hide();
+}
+
+function formatMessage(type, value) {
+  return {type, value};
+}
 
 /**
  * Set the register and update graph
  */
 function setVotes(value){
-  sondage.setValue(value);
-  changeData(sondage.getValue());
+  register.sondage = value;
+  changeData(register.sondage);
 }
 
 /**
@@ -107,8 +150,9 @@ function setVotes(value){
  * and update graph
  */
 function addYes(){
-  value = sondage.getValue();
+  value = register.sondage;
   setVotes([++value[0],value[1]]);
+  broadcast(register);
 }
 
 /**
@@ -116,14 +160,21 @@ function addYes(){
  * and update graph
  */
 function addNo(){
-  value = sondage.getValue();
+  value = register.sondage;
   setVotes([value[0],++value[1]]);
+  broadcast(register);
+}
+
+function broadcast(reg){
+  // broadcast the register to all peers that we changed the register
+  f.sendBroadcast(formatMessage('update', reg));
 }
 
 /*
 * Update the canvas with new values
 */
 function changeData(value){
+  console.log('Change Data: ', value);
   $('#yesLabel').html("Oui:" + value[0]);
   $('#noLabel').html("Non:" + value[1]);
   Donut3D.transition("quotesDonut", updateData(value), 130, 100, 30, 0);
